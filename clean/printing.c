3b54a99fa7f820ce3cbc636a31e1dba4cfb10aaa
Lars Heinrichs <lars.heinrichs@web.de>
2020-03-05 21:51:44 +0100

    Improve user input struct for readability

---
+++ clean/printing.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void  print_horizon_distance(float distance);
+
+void print_horizon_distance(float distance) {
+    printf("\nThe horizon on a calm ocean is %.2f km far away.\n", distance);
+}
